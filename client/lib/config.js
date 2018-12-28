06e9df0f7f0f45ee95efc1d86b2a219c142e33c3
Adam <geekosoft@live.co.uk>
2018-12-28 15:30:05 +0000

    _Always_ download song-loader

--- client/lib/config.js
+++ client/lib/config.js
@@ -1,8 +1,9 @@
 const path = require('path')
 const fs = require('fs')
 const log = require('./log')
-const configLocation = path.join(process.cwd(), 'bpm.json')
-const pluginsLocation = path.join(process.cwd(), 'bpmPlugins.txt')
+const CONFIG_PATH = path.join(process.cwd(), 'bpm.json')
+const PLUGINS_PATH = path.join(process.cwd(), 'bpmPlugins.txt')
+const REQUIRED_PLUGINS = ['song-loader']
 
 function guessPlatform (directory) {
   let d = directory.toLowerCase()
@@ -12,9 +13,9 @@ function guessPlatform (directory) {
 }
 
 function getConfig () {
-  if (fs.existsSync(configLocation)) {
+  if (fs.existsSync(CONFIG_PATH)) {
     // bpm.json is... JSON
-    return JSON.parse(fs.readFileSync(configLocation, 'utf8'))
+    return JSON.parse(fs.readFileSync(CONFIG_PATH, 'utf8'))
   } else {
     log.say('ERROR', `Couldn't find bpm.json, making one up.`)
     log.say('WARNING', `Platform assumed as 'steam'`)
@@ -27,15 +28,15 @@ function getConfig () {
 }
 
 function getPlugins () {
-  if (fs.existsSync(pluginsLocation)) {
+  if (fs.existsSync(PLUGINS_PATH)) {
     // bpmPlugins.txt is split by newline
-    var p = fs.readFileSync(pluginsLocation, 'utf8').split(/[\r\n]+/)
+    var p = fs.readFileSync(PLUGINS_PATH, 'utf8').split(/[\r\n]+/)
     p = p.filter((x) => x !== '')
-    return p
+    return REQUIRED_PLUGINS.concat(p)
   } else {
     log.say('WARNING', `Couldn't find bpmPlugins.txt, bpm will only update song-loader.`)
     // Why do we need song-loader? It contains IPA.
-    return ['song-loader']
+    return REQUIRED_PLUGINS
   }
 }
 
@@ -45,7 +46,7 @@ module.exports.plugins = getPlugins()
 module.exports.save = () => {
   // Beautify using 2 space tabs. Easier for humans to edit.
   var configString = JSON.stringify(module.exports.config, null, 2)
-  fs.writeFile(configLocation, configString, (err) => {
+  fs.writeFile(CONFIG_PATH, configString, (err) => {
     if (err) {
       log.say('ERROR', `Couldn't save bpm.json`)
       log.err(err)
